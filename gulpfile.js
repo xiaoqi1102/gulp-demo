47c7fb2d68d4094ad8898447f1f44707a35fb446
xiaoqi1102 <705764065@qq.com>
2016-06-02 17:10:53 +0800

    测试 gulp-less
    数组多文件编译

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,16 @@
  * Created by yzsoft on 16/6/2.
  */
 var gulp=require('gulp'),
-    uglify=require('gulp-uglify');
+    uglify=require('gulp-uglify'),
+    less=require('gulp-less');
 gulp.task('jsmin',function(){
    gulp.src('src/js/index.js')
        .pipe(uglify())
        .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('testLess',function(){
+    gulp.src(['src/less/style.less','src/less/page.less'])
+        .pipe(less())
+        .pipe(gulp.dest('dist/css'))
 });
