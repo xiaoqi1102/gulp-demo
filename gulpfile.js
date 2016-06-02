82012219fb26e3ca9cfcb4e8f4f6ef1d0da95085
xiaoqi1102 <705764065@qq.com>
2016-06-02 17:00:40 +0800

    新增压缩测试配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,10 @@
 /**
  * Created by yzsoft on 16/6/2.
  */
+var gulp=require('gulp'),
+    uglify=require('gulp-uglify');
+gulp.task('jsmin',function(){
+   gulp.src('src/js/index.js')
+       .pipe(uglify())
+       .pipe(gulp.dest('dist/js'))
+});
